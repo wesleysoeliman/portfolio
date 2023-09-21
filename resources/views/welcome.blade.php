118c723d2f8fbe633c18d1cdd97d2716d0d1f625
wesleysoeliman <wesley.soeliman@gmail.com>
2023-09-21 23:27:09 +0200

    frontend bijwerken

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,103 +1,70 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        </h2>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Portfolio</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="antialiased">
+    <header class="bg-blue-400 text-white py-4">
+        <div class="container mx-auto px-4 flex justify-between items-center"> <!-- Added px-4 for padding -->
+            <h1 class="text-2xl font-semibold">Portfolio</h1>
+            <ul class="flex space-x-4">
+                <li><a href="#" class="hover:underline">Home</a></li>
+                <li><a href="#" class="hover:underline">About me</a></li>
+                <li><a href="#" class="hover:underline">Hobbies</a></li>
+                <li><a href="#" class="hover:underline">Contact</a></li>
+            </ul>
+        </div>
+    </header>
+    <header class="bg-blue-600 text-white py-16 text-center">
+        <h1 class="text-4xl font-semibold">Welkom op mijn pagina</h1>
+        <p class="mt-4 text-lg">web development.</p>
+    </header>
+    <div class="p-6 border-t border-gray-100 md:border-t-0 md:border-l">
+        <h2 class="text-3xl font-semibold mb-8 text-center">About me</h2>
+        <div class="flex justify-center items-center ml-12">
+            <p>
+                Hello, I'm [Your Name]. I am a passionate web developer with expertise in Laravel, PHP, and JavaScript. I love building web applications and exploring new technologies. Feel free to get in touch with me to discuss your next project!
+                Hello, I'm [Your Name]. I am a passionate web developer with expertise in Laravel, PHP, and JavaScript. I love building web applications and exploring new technologies. Feel free to get in touch with me to discuss your next project!
+            </p>
+        </div>
+    </div>
     
-        <!-- Styles -->
-        <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--tw-bg-opacity: 1;background-color:rgb(255 255 255 / var(--tw-bg-opacity))}.bg-gray-100{--tw-bg-opacity: 1;background-color:rgb(243 244 246 / var(--tw-bg-opacity))}.border-gray-200{--tw-border-opacity: 1;border-color:rgb(229 231 235 / var(--tw-border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{--tw-shadow: 0 1px 3px 0 rgb(0 0 0 / .1), 0 1px 2px -1px rgb(0 0 0 / .1);--tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color), 0 1px 2px -1px var(--tw-shadow-color);box-shadow:var(--tw-ring-offset-shadow, 0 0 #0000),var(--tw-ring-shadow, 0 0 #0000),var(--tw-shadow)}.text-center{text-align:center}.text-gray-200{--tw-text-opacity: 1;color:rgb(229 231 235 / var(--tw-text-opacity))}.text-gray-300{--tw-text-opacity: 1;color:rgb(209 213 219 / var(--tw-text-opacity))}.text-gray-400{--tw-text-opacity: 1;color:rgb(156 163 175 / var(--tw-text-opacity))}.text-gray-500{--tw-text-opacity: 1;color:rgb(107 114 128 / var(--tw-text-opacity))}.text-gray-600{--tw-text-opacity: 1;color:rgb(75 85 99 / var(--tw-text-opacity))}.text-gray-700{--tw-text-opacity: 1;color:rgb(55 65 81 / var(--tw-text-opacity))}.text-gray-900{--tw-text-opacity: 1;color:rgb(17 24 39 / var(--tw-text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--tw-bg-opacity: 1;background-color:rgb(31 41 55 / var(--tw-bg-opacity))}.dark\:bg-gray-900{--tw-bg-opacity: 1;background-color:rgb(17 24 39 / var(--tw-bg-opacity))}.dark\:border-gray-700{--tw-border-opacity: 1;border-color:rgb(55 65 81 / var(--tw-border-opacity))}.dark\:text-white{--tw-text-opacity: 1;color:rgb(255 255 255 / var(--tw-text-opacity))}.dark\:text-gray-400{--tw-text-opacity: 1;color:rgb(156 163 175 / var(--tw-text-opacity))}.dark\:text-gray-500{--tw-text-opacity: 1;color:rgb(107 114 128 / var(--tw-text-opacity))}}
-        </style>
-    </head>
-    <body class="antialiased">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                  
-                </div>
-            @endif
-
-            <header class="bg-blue-400 text-white py-4">
-                <div class="container mx-auto flex justify-between items-center">
-                    <h1 class="text-2xl font-semibold">Portfolio</h1>
-                    <ul class="flex space-x-4">
-                      
-                        <li><a href="#" class="hover:underline">Home</a></li>
-                        <li><a href="#" class="hover:underline">Over mij</a></li>
-                        <li><a href="#" class="hover:underline">Portfolio</a></li>
-                        <li><a href="#" class="hover:underline">Contact</a></li>
-                        @auth
-                        <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
-                    @else
-                        <li><a href="{{ route('login') }}"></a></li>
-                        @if (Route::has('register'))
-                           <li><a href="{{ route('register') }}">Register</a></li>
-                        @endif
-                    @endauth
-                    </ul>
-                </div>
-            </header>
-                <header class="bg-blue-600 text-white py-16 text-center">
-                    <h1 class="text-4xl font-semibold">Welkom op mijn pagina</h1>
-                    <p class="mt-4 text-lg">web development.</p>
-                    <a href="#">
-                </header>
-                <div class="p-6 border-t border-gray-100 dark:border-gray-700 md:border-t-0 md:border-l">
-                        <h2 class="text-3xl font-semibold mb-8">About me</h2>
-                    </div>
-                    
-                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                            <div class="p-6 text-gray-900">
-                    <div class="ml-12">
-                        
-                            Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                        </div>
-                    </div>
-                    <section class="bg-gray-200 py-16">
-                        <div class="container mx-auto text-center">
-                            <h2 class="text-3xl font-semibold mb-8">Hobbies</h2>
-                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
-                                <!-- Hobby Block 1 -->
-                                <div class="bg-white p-4 rounded-lg shadow-lg">
-                                    <h3 class="text-xl font-semibold mb-2">Hobby 1</h3>
-                                    <p class="text-gray-400">Hobby 2</p>
-                                </div>
-                                                              <!-- Hobby Block 2 -->
-                            <div class="bg-white p-4 rounded-lg shadow-lg">
-                                <h3 class="text-xl font-semibold mb-2">Hobby 1</h3>
-                                <p class="text-gray-400">Hobby 2</p>
-                            </div>
-                            </div>
-
-
-                        </div>
-
-
-                    <div class="ml-12">
-                        <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                            Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                        </div>
-                    </div>
-                </div>
-
-                </body>
-              
-                </div>
-            
-           
-                <footer class="bg-blue-600 text-white py-4 text-center w-full">
-                    <div class="container mx-auto">
-                        <p>&copy; 2023 Wesley</p>
-                    </div>
-                </footer>     
-    </body>
+  
+    
+    <div class="container mx-auto text-center mt-15">
+        <div class="bg-blue-600 text-white py-16 text-center">
+        <h2 class="text-3xl font-semibold mb-8">Hobbies</h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+            <!-- Hobby Block 1 -->
+            <div class="bg-white p-4 rounded-lg shadow-lg">
+                <h3 class="text-gray-700 font-semibold mb-2">Hobby 1</h3>
+                <p class="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+            </div>
+            <!-- Hobby Block 2 -->
+            <div class="bg-white p-4 rounded-lg shadow-lg">
+                <h3 class="text-gray-700 font-semibold mb-2">Hobby 2</h3>
+                <p class="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit...</p>
+            </div>
+            <!-- Hobby Block 3 -->
+            <div class="bg-white p-4 rounded-lg shadow-lg">
+                <h3 class="text-gray-700 font-semibold mb-2">Hobby 3</h3>
+                <p class="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit...</p>
+            </div>
+            <!-- Hobby Block 4 -->
+            <div class="bg-white p-4 rounded-lg shadow-lg">
+                <h3 class="text-gray-700 font-semibold mb-2">Hobby 4</h3>
+                <p class="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit...</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <footer class="bg-blue-600 text-white py-4 text-center mt-20"> <!-- margin top 20 -->
+        <div class="container mx-auto">
+            <p>&copy; 2023 Wesley</p>
+        </div>
+    </footer>
+</body>
 </html>
